6d063a1835d2e15751ec273584eefe00809a2f19
Greg Hsu <guppoe238@gmail.com>
2021-03-05 00:42:30 +0800

    add filter 100+

--- reviews_analytics.py
+++ reviews_analytics.py
@@ -10,10 +10,24 @@ with open('reviews.txt', 'r') as f:
 		if count % 1000 == 0: # 每1000筆資料，即相當於count除以1000餘0時
 			print (len(data)) # 印出長度
 							  # 如此一來可以看到讀取進度
+	print('讀取完了，一共有', len(data), '筆資料')
 
 # 計算留言平均長度
+
 letter_count = 0
 for d in data:
 	letter_count += len(d)
 
+
 print('留言的平均長度為: ', letter_count / len(data))
+
+#篩選留言長度大於100字者
+
+within100 = []
+for d in data:
+	if len(d) < 100:
+		within100.append(d)
+
+print('一共有', len(within100), '筆資料的留言長度小於100')
+print(within100[0]) # 印出第一筆小於100的資料看看
+
